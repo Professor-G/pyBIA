91a25bfbbcaae1f54a63c1672aa866af6d38450e
Daniel <daniel@Daniels-MacBook-Pro.local>
2021-10-29 01:12:20 -0700

    for loop fix

--- pyBIA/data_augmentation.py
+++ pyBIA/data_augmentation.py
@@ -51,6 +51,7 @@ def resize(data, size=50):
         height = data.shape[1]
     else:
         raise ValueError("Channel cannot be one dimensional")
+
     if width != height:
         raise ValueError("Can only resize square images")
     if width == size:
@@ -58,24 +59,29 @@ def resize(data, size=50):
         if len(data.shape) == 4:
             data = data[:, :, :, 0]
         return data
+
     if len(data.shape) == 2:
         resized_data = fixed_size_subset(np.array(np.expand_dims(data, axis=-1))[:, :, 0], int(width/2.), int(height/2.), size)
         return resized_data
     else:
         resized_images = []    
         for i in np.arange(0, len(data)):
-            resized_data = fixed_size_subset(data[i][:, :, 0], int(width/2.), int(height/2.), size)
+            if len(data[i].shape) == 2:
+                resized_data = fixed_size_subset(np.array(np.expand_dims(data[i], axis=-1))[:, :, 0], int(width/2.), int(height/2.), size)
+            else:
+                resized_data = fixed_size_subset(data[i][:, :, 0], int(width/2.), int(height/2.), size)
             resized_images.append(resized_data)
+
     resized_data = np.array(resized_images)
     return resized_data
 
-def augmentation(data, batch=10, image_width=50):
+def augmentation(data, batch=10, image_width=50, rotation=180, width=0.05, height=0.05, horizontal=True, vertical=True, fill='nearest'):
     """
     Performs data augmentation on 
     non-normalized data and 
     resizes image to 50x50
     """
-    rotation, width, height, horizontal, vertical, fill = generator_parameters()[:6]
+
     datagen = ImageDataGenerator(
         rotation_range=rotation,
         width_shift_range=width,
@@ -83,18 +89,22 @@ def augmentation(data, batch=10, image_width=50):
         horizontal_flip=horizontal,
         vertical_flip=vertical,
         fill_mode=fill)
+
     if len(data.shape) != 4:
         if len(data.shape) == 3 or len(data.shape) == 2:
             data = np.array(np.expand_dims(data, axis=-1))
         else:
             raise ValueError("Input data must be 2D for single sample or 3D for multiple samples")
+
     augmented_data = []
     for i in np.arange(0, len(data)):
         original_data = data[i].reshape((1,) + data[-i].shape)
         for k in range(batch):
         	augement = datagen.flow(original_data, batch_size=1)
         	augmented_data.append(augement[0][0])
+
     augmented_data = np.array(augmented_data)
     augmented_data = resize(augmented_data, size=image_width)
+
     return augmented_data
 

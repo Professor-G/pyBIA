276636d8990a51c6c96eae715d97efb89dfc9790
Daniel <daniel@Daniels-MBP.MG8702>
2021-11-28 17:50:06 -0700

    added checkpoint in case training is interrupted

--- pyBIA/pyBIA.py
+++ pyBIA/pyBIA.py
@@ -14,13 +14,15 @@ from tensorflow.keras.initializers import VarianceScaling
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.layers import Activation, Dense, Dropout, Conv2D, MaxPool2D, Flatten, BatchNormalization
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 from data_processing import process_class, create_training_set
 
 
 def pyBIA_model(blob_data, other_data, img_num_channels=1, normalize=True, min_pixel=638, max_pixel=3000, 
     validation_X=None, validation_Y=None, epochs=100, batch_size=32, lr=0.0001, momentum=0.9, decay=0.0005,
-    nesterov=False, loss='categorical_crossentropy', padding='same', dropout=0.5, pooling=True, metrics=True, filename=''):
+    nesterov=False, loss='categorical_crossentropy', padding='same', dropout=0.5, pooling=True, metrics=True, 
+    checkpoint=True, filename=''):
     """
     The CNN model infrastructure presented by AlexNet, with
     modern modifications.
@@ -94,6 +96,10 @@ def pyBIA_model(blob_data, other_data, img_num_channels=1, normalize=True, min_p
 
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
     
+    if checkpoint:
+        model_checkpoint = ModelCheckpoint("checkpoint.hdf5", monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+        callbacks_list = [model_checkpoint]
+
     if validation_X is None:
         history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=1)
     elif validation_X is not None:

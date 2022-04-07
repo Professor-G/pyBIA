db474d4a95696f32088a53b3db3b2531a4c96390
Daniel <daniel@Daniels-MBP-5.MG8702>
2022-04-07 14:27:25 -0600

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name="pyBIA",
-    version="0.9.6",
+    version="0.9.8",
     author="Daniel Godines",
     author_email="danielgodinez123@gmail.com",
     description="Convolutional Neural Network for Ly-alpha blob detection",

7641d6e377f8a6c436dcedbe73239fab15d4e8e9
Daniel <daniel@Daniels-MBP-5.MG8702>
2022-06-29 13:58:52 -0600

    new version, limiting CNN search

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name="pyBIA",
-    version="1.0.6",
+    version="1.0.7",
     author="Daniel Godines",
     author_email="danielgodinez123@gmail.com",
     description="Convolutional Neural Network for Ly-alpha Blob Detection",

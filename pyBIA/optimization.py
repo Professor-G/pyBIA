562e6ca8a66d4af92661012527cd8bbe80363907
Daniel <daniel@godinesd705.nmsu.edu>
2023-05-01 10:58:38 -0600

    adding metric2  to track addtl early stopping

--- pyBIA/optimization.py
+++ pyBIA/optimization.py
@@ -175,7 +175,7 @@ class objective_cnn(object):
     """
 
     def __init__(self, positive_class, negative_class, val_positive=None, val_negative=None, img_num_channels=1, clf='alexnet', 
-        normalize=True, min_pixel=0, max_pixel=1000, patience=5, metric='loss', average=True, 
+        normalize=True, min_pixel=0, max_pixel=1000, patience=5, metric='loss', metric2=None, average=True, 
         test_positive=None, test_negative=None, batch_size_min=16, batch_size_max=64, opt_model=True, train_epochs=25, opt_cv=None,
         opt_aug=False, batch_min=2, batch_max=25, batch_other=1, balance=True, image_size_min=50, image_size_max=100, shift=10, opt_max_min_pix=None, opt_max_max_pix=None, 
         mask_size=None, num_masks=None, smote_sampling=0, blend_max=0, num_images_to_blend=2, blending_func='mean', blend_other=1, 
@@ -208,6 +208,7 @@ class objective_cnn(object):
         self.opt_max_min_pix = opt_max_min_pix
         self.opt_max_max_pix = opt_max_max_pix
         self.metric = metric 
+        self.metric2 = metric2
         self.average = average
         self.shift = shift 
         self.opt_cv = opt_cv
@@ -406,6 +407,11 @@ class objective_cnn(object):
             else:
                 callbacks = None
 
+        if self.metric2 is not None:
+            mode2 = 'min' if 'loss' in self.metric2 else 'max'
+            callbacks.append(EarlyStopping(monitor=self.metric2, mode=mode2, patience=self.patience))
+            print('Setting additional early stopping criteria: {}'.format(self.metric2))
+            
         ### ### ### ### ### ### ### ### ### 
              ## Optimize CNN Model ##
         ### ### ### ### ### ### ### ### ### 
@@ -1375,7 +1381,7 @@ class Monitor_Tracker(Callback):
 
 
 def hyper_opt(data_x=None, data_y=None, val_X=None, val_Y=None, img_num_channels=1, clf='alexnet', 
-    normalize=True, min_pixel=0, max_pixel=1000, n_iter=25, patience=5, metric='loss', average=True, 
+    normalize=True, min_pixel=0, max_pixel=1000, n_iter=25, patience=5, metric='loss', metric2=None, average=True, 
     test_positive=None, test_negative=None, opt_model=True, batch_size_min=16, batch_size_max=64, train_epochs=25, opt_cv=None,
     opt_aug=False, batch_min=2, batch_max=25, batch_other=1, balance=True, image_size_min=50, image_size_max=100, shift=10, opt_max_min_pix=None, opt_max_max_pix=None, 
     mask_size=None, num_masks=None, smote_sampling=0, blend_max=0, num_images_to_blend=2, blending_func='mean', blend_other=1, zoom_range=(0.9,1.1), skew_angle=0,
@@ -1665,7 +1671,7 @@ def hyper_opt(data_x=None, data_y=None, val_X=None, val_Y=None, img_num_channels
        
     else:
         objective = objective_cnn(data_x, data_y, val_positive=val_X, val_negative=val_Y, img_num_channels=img_num_channels, clf=clf, 
-            normalize=normalize, min_pixel=min_pixel, max_pixel=max_pixel, patience=patience, metric=metric, average=average,  
+            normalize=normalize, min_pixel=min_pixel, max_pixel=max_pixel, patience=patience, metric=metric, metric2=metric2, average=average,  
             test_positive=test_positive, test_negative=test_negative, opt_model=opt_model, batch_size_min=batch_size_min, batch_size_max=batch_size_max, train_epochs=train_epochs, opt_cv=opt_cv,
             opt_aug=opt_aug, batch_min=batch_min, batch_max=batch_max, batch_other=batch_other, balance=balance, image_size_min=image_size_min, image_size_max=image_size_max, 
             shift=shift, opt_max_min_pix=opt_max_min_pix, opt_max_max_pix=opt_max_max_pix, mask_size=mask_size, num_masks=num_masks, smote_sampling=smote_sampling, 
